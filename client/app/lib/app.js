fb006dc77d2be9283f79320914cc7b70fef7f3f5
Tally Barak <tally.barak@gmail.com>
2015-09-28 12:45:30 +0300

    folder restructure

--- client/app/lib/app.js
+++ client/app/lib/app.js
@@ -15,7 +15,3 @@ if (Meteor.isCordova)
     angular.element(document).on('deviceready', onReady);
 else
     angular.element(document).ready(onReady);
-
-//Accounts.ui.config({
-//    passwordSignupFields: "USERNAME_ONLY"
-//});
